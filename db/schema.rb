2d2ba855d42a19323c406805c3bcd49de8252fbf
mashype <mashype@me.com>
2016-09-07 11:14:50 -0700

    update the edit task page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831145033) do
+ActiveRecord::Schema.define(version: 20160831203958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,8 @@ ActiveRecord::Schema.define(version: 20160831145033) do
     t.date     "due_date"
     t.integer  "priority_id"
     t.integer  "disposition_id"
+    t.string   "description"
+    t.string   "next_steps"
   end
 
   add_index "tasks", ["disposition_id"], name: "index_tasks_on_disposition_id", using: :btree
